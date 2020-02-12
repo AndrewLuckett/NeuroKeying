494e9099e5e90c1d1ec8881e7c1b21a408dd5d4b
Andrew Luckett <andyluckett9@gmail.com>
2020-02-12 19:32:31 +0000

    Add start to net learning

--- src/Activators.py
+++ src/Activators.py
@@ -1,32 +1,73 @@
-from math import exp #Global since sigmoid may be called a lot
+from math import exp  # Global since sigmoid may be called a lot
 
-def __listAct(inp,func): #Bad code maybe
-    #But I wanted the functions themselves to look clean
-    out = []
-    for i in inp:
-        out.append(func(i))
-    return out
+"""
+Pattern of:
 
-def linear(inp):
-    if type(inp) == list:
-        return __listAct(inp,linear)
+class name(Activator):
+    def activator(in):
+        return out
+
+    def derivative(in):
+        return out
+
+more so static than OO
+Just a grouping of two related funcs
+functional prog?
+"""
+
+
+class Activator:  
+    def __listAct__(this, inp, func):
+        out = []
+        for i in inp:
+            out.append(func(i))
+        return out
+
+
+class Linear(Activator):
+    def activator(this,inp):
+        if type(inp) == list:
+            return this.__listAct__(inp, this.activator)
+
+        if inp > 1:
+            return 1
+        if inp < 0:
+            return 0
+        return inp
+
+
+    def derivative(this,inp):
+        if type(inp) == list:
+            return this.__listAct__(inp, this.derivative)
+        return None
+
+
+class Sigmoid(Activator):
+    def activator(this, inp):
+        if type(inp) == list:
+            return this.__listAct__(inp, this.activator)
         
-    if inp > 1:
-        return 1
-    if inp < 0:
-        return 0
-    return inp
+        return 1 / (1 + exp(-inp))
 
-def sigmoid(inp):
-    if type(inp) == list:
-        return __listAct(inp,sigmoid)
 
-    return 1 / ( 1 + exp(-inp))
+    def derivative(this, inp):
+        if type(inp) == list:
+            return this.__listAct__(inp, this.derivative)
+        return None
 
-def rectlin(inp): #Linear but uncapped positive
-    if type(inp) == list:
-        return __listAct(inp,rectlin)
+
+class Rectlin(Activator):
+    def activator(this, inp):
+        if type(inp) == list:
+            return this.__listAct__(inp, this.activator)
         
-    if inp < 0:
-        return 0
-    return inp
+        if inp < 0:
+            return 0
+        return inp
+
+
+    def derivative(this, inp):
+        if type(inp) == list:
+            return this.__listAct__(inp, this.derivative)
+        return None
+
--- src/Neuro.py
+++ src/Neuro.py
@@ -20,35 +20,65 @@ class Net:
         elif kwargs.get("dimensions"): #list of ints
             #creates empty matrices of depth int
             this.dimension = kwargs.get("dimensions")
-            for i in range(len(this.dimension)-1):
+            for i in range(len(this.dimension) - 1):
                 this.layers.append(
-                    Matrix((this.dimension[i+1],this.dimension[i]+1)))
+                    Matrix((this.dimension[i + 1], this.dimension[i] + 1)))
                 
         else: #Needs some kind of init data
             raise Exception("Insuficient arguments")
 
         this.learningRate = kwargs.get("learningRate") or 0.2
-        this.activator = kwargs.get("activator") or Activators.linear
+        this.activator = kwargs.get("activator") or Activators.Linear()
                 
-    def getOutput(this,inp): #Dont forget bias value
+    def getOutput(this, inp): #Dont forget bias value
         if type(inp) == list:
-            inp = Matrix((len(inp),1),inp)
+            inp = Matrix((len(inp), 1), inp)
         if type(inp) != Matrix:
             raise Exception("Invalid argument type")
-        if inp.getSize() != (this.dimension[0]+1,1):
+        if inp.getSize() != (this.dimension[0] + 1, 1):
             raise Exception("Bad input size")
 
         for layer in this.layers:
             #print("P: "+str(inp))
             out = layer * inp
             out = out.getRaw() #Get as array
-            out = this.activator(out) #Apply activator
-            out.insert(0,1.0) #Add 1 at at start for bias
+            out = this.activator.activator(out) #Apply activator
             
-            inp = Matrix((len(out),1),out) #turn into matrix and overwrite inp
+            inp = out.copy() #Save out for output incase last and temp use inp
+            inp.insert(0, 1.0) #Add 1 at at start as bias for next
+            inp = Matrix((len(inp), 1), inp) #turn into matrix and overwrite inp
 
-        return inp
+        return Matrix((len(out), 1), out)
 
-    def learn(this,inp,out):
+    def learn(this, inp, targetOut):
+        if type(inp) == list:
+            inp = Matrix((len(inp), 1), inp)
+        if type(targetOut) == list:
+            targetOut = Matrix((len(targetOut), 1), targetOut)
+        if type(inp) != Matrix or type(targetOut) != Matrix:
+            raise Exception("Invalid argument type")
+        if inp.getSize() != (this.dimension[0] + 1, 1):
+            raise Exception("Bad input size")
+        if targetOut.getSize() != (this.dimension[-1], 1):
+            raise Exception("Bad output size")
 
-        return True
+        outs = []
+
+        for layer in this.layers:
+            #print("P: "+str(inp))
+            out = layer * inp
+            out = out.getRaw() #Get as array
+            out = this.activator.activator(out) #Apply activator
+
+            outs.append(Matrix((len(out), 1), out))
+            
+            inp = out.copy() #Save out for output incase last and temp use inp
+            inp.insert(0, 1.0) #Add 1 at at start as bias for next
+            inp = Matrix((len(inp), 1), inp) #turn into matrix and overwrite inp
+
+        print("To do")
+        return outs
+
+
+
+
